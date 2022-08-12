5b1edaeb39a008eae593b122b2eca1256d5fb9e6
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2022-08-11 22:21:30 -0300

    3ª Atualização

---
+++ Mao_Na_Massa06/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mao_Na_Massa06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando Condicional");
+            
+            int idade = 18;
+            int acompanhado = 3;
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("Você tem mais que 18 anos");
+                Console.WriteLine("Seja bem vindo ");
+
+            }
+            else
+            {
+                if (acompanhado >= 3)
+                    {
+                    Console.WriteLine("Você não tem 18 anos,mas pode entrar, pois esta acompanhado ");
+                }
+                else
+                {
+                    Console.WriteLine("Infelizmente você não pode entrar");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
