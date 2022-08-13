afcdba36caaec0647398809af28d0ee5ac2cc12c
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2022-08-13 18:53:51 -0300

    9ª Atualização

---
+++ Mao_Na_Massa07/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mao_Na_Massa07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Mão Na Massa 08");
+
+            int contador = 10;
+
+            while (contador <=50)
+            {
+                Console.WriteLine("Contagem " + contador);
+
+                    contador++;
+            }
+            
+            
+            
+            
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
