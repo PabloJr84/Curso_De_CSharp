709fa592c871fdf2d0bac2953d5e9a5c8ea87a59
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2022-08-13 01:42:42 -0300

    5ª Atualização

---
+++ 9-Escopo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Escopo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 9  Escopo");
+
+            int idadeJoao = 18;
+            bool acompanhado = false;
+            string mensagemAdicional;
+
+            if (acompanhado == true)
+            {
+                mensagemAdicional = "João está acompanhado! ";
+            }
+
+            else
+            {
+                mensagemAdicional = "João não está acompanhado";
+            }
+         
+            if (idadeJoao >= 18 || acompanhado == true)
+            {
+                Console.WriteLine("Pode entrar. ");
+                Console.WriteLine(mensagemAdicional);
+            }
+
+         
+            else
+            {
+                Console.WriteLine("Não pode entrar");
+            }
+
+
+            Console.WriteLine("Aperte enter para sair");
+            Console.ReadLine();
+        }
+    }
+}
