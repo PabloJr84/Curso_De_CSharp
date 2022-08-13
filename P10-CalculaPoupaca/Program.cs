f748b23a4c64b06426b6fe575b181a67f8d7fc34
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2022-08-13 17:28:31 -0300

    6ª Atualização

---
+++ P10-CalculaPoupaca/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P10_CalculaPoupaca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 10 - Caula Poupança");
+
+
+            double valorinvestido = 1000;
+
+            //0.36% = 0.0036
+            valorinvestido = valorinvestido + valorinvestido * 0.0036;
+            Console.WriteLine("Após 1 mês, você terá R$" + valorinvestido);
+            
+            
+            
+            
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
