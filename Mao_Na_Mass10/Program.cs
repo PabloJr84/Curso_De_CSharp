20b48d343b4b61b1a1cf9d55b087fa1475d5e8d9
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2022-08-15 21:25:37 -0300

    11ª Atualização

---
+++ Mao_Na_Mass10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mao_Na_Massa10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Laco De Repeticao For");
+
+            for (int multiplicador = 1; multiplicador <= 10; multiplicador++)
+            {
+                for (int contador = 0; contador <=10; contador++)
+                {
+                    Console.WriteLine(multiplicador + " * " + contador + " = " + multiplicador * contador);
+                    
+                }
+            Console.WriteLine();
+            }
+
+            
+            Console.ReadLine();
+        }
+    }
+}
