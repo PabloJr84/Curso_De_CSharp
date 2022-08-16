54b42ea13b1167d408c31022d348d7c31ddd9106
PABLO JUNIOR MOREIRA DE ASSIS <52177225+PabloJr84@users.noreply.github.com>
2022-08-15 21:10:33 -0300

    10ª Atualização

---
+++ Mao_Na_Massa09/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mao_Na_Massa09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mão Na Massa 09");
+            
+            int contador = 0;
+
+            while (contador <=10)
+            {
+                contador++;
+                
+                Console.WriteLine("Contador = " + contador);
+            }
+            
+            
+            
+            
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ P13-Encadeado/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P13_Encadeado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 13");
+
+            for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for( int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                    {
+                    Console.Write("*");
+
+                    if (contadorColuna >= contadorLinha)
+                        break;
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
